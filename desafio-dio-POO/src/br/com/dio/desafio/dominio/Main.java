dbb98686ad3fb1e0ad18fed4d727136bad933a98
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-26 18:11:17 -0300

    Conceitos de Herança e Polimorfismo - pt 01

--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Main.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Main.java
@@ -9,23 +9,34 @@ public class Main {
        curso1.setTitulo("BootCamp");
         curso1.setDescricao("Curso 01");
         curso1.setCargaHoraria(20);
+ 
+
 
         Curso cursoDelta = new Curso();
-
         cursoDelta.setTitulo("Curso Delta");
         cursoDelta.setDescricao("curso 02");
         cursoDelta.setCargaHoraria(30);
+
 
         Mentoria mentoria01 = new Mentoria();
         mentoria01.setTitulo("Mentoria Delta-13");
         mentoria01.setDescricao("Mentoria 01");
         mentoria01.setData(LocalDate.now());
 
+
+
+        Conteudo conteudo = new Curso();
+        conteudo.setTitulo("Conteúdo de aprendizagem");
+        conteudo.setDescricao("Conceitos de Polimorfismo");
+
+
         System.out.println(curso1);
 
         System.out.println(cursoDelta);
-        
+
         System.out.println(mentoria01);
+
+        System.out.println(conteudo);
 
 
     }
--- desafio-dio-POO/src/br/com/dio/desafio/dominio/Mentoria.java
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,38 +3,20 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 import java.util.Date;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     private LocalDate data;
 
-    public Mentoria(String titulo, String descricao, LocalDate data) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.data = data;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20;
     }
 
     public Mentoria() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
     public LocalDate getData() {
         return data;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public void setData(LocalDate data) {
@@ -44,8 +26,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria {" +
-                "titulo: " + titulo +
-                ", descricao: " + descricao +
+                "titulo: " + getTitulo() +
+                ", descricao: " +getDescricao() +
                 ", data: " + data +
                 " }";
     }
