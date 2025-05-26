7d6d0fa277914f5f5efe6279402b73413b30de31
Thiago Renan <thiagorenan1108@gmail.com>
2025-05-25 21:22:29 -0300

    Classes criadas - Curso / Mentoria

---
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+       curso1.setTitulo("BootCamp");
+        curso1.setDescricao("Curso 01");
+        curso1.setCargaHoraria(20);
+
+        Curso cursoDelta = new Curso();
+
+        cursoDelta.setTitulo("Curso Delta");
+        cursoDelta.setDescricao("curso 02");
+        cursoDelta.setCargaHoraria(30);
+
+        Mentoria mentoria01 = new Mentoria();
+        mentoria01.setTitulo("Mentoria Delta-13");
+        mentoria01.setDescricao("Mentoria 01");
+        mentoria01.setData(LocalDate.now());
+
+        System.out.println(curso1);
+
+        System.out.println(cursoDelta);
+        
+        System.out.println(mentoria01);
+
+
+    }
+}
